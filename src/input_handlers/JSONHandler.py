55593b032858a093e754ae0f94f87976ed45917c
kaniz3 <kanizk2002@hotmail.com>
2024-11-09 19:28:05 +1100

    Push Binaries

--- src/input_handlers/JSONHandler.py
+++ src/input_handlers/JSONHandler.py
@@ -11,8 +11,11 @@ class JSONHandler:
             JSONHelper.simple_buffer_overflow,
         ]
         
-        # Apply individual fuzzing options
+        # Append original input
         fuzzed = []
+        fuzzed.append(json_input) 
+        
+        # Apply individual fuzzing options
         for fuzz in fuzzing_options:
             fuzzed.append(fuzz(json_input))
         
@@ -25,16 +28,18 @@ class JSONHandler:
         fmts = JSONHelper.get_format_str()
         for fmt in fmts:
             fuzzed.append(JSONHelper.put_format_str(json_input, fmt))
-
+            
+            
         # Apply byte flips
-        for i in range(40):
-            fuzzed.append(JSONHelper.byte_flip_string(json_input)) 
+        # for i in range(40):
+        #     fuzzed.append(JSONHelper.byte_flip_string(json_input)) 
 
         # Apply byte flips
         #for i in range(40):
         #    fuzzed.append(JSONHelper.byte_flip(json_input)) 
             
-        return fuzzed
+        fuzzed_bytes = [json.dumps(item).encode() if isinstance(item, dict) else item for item in fuzzed]    
+        return fuzzed_bytes
 
     @staticmethod
     def parse_input(content):
@@ -42,13 +47,15 @@ class JSONHandler:
         Parses JSON and returns list of possible input dictionaries for harness.
         """
         data = json.loads(content)
+
+        fuzzed = []
         if isinstance(data, list):
-            fuzzed = []
             for item in data:
                 fuzzed.extend(JSONHandler.mutate(item))
-            return fuzzed_inputs
         else:
-            return JSONHandler.mutate(data)
+            fuzzed.extend(JSONHandler.mutate(data))
+            
+        return fuzzed
     
     @staticmethod
     def send_json():
