864a0a80d217f47fe7a6a93626c05427c79269ea
Amy <149349016+ffordian@users.noreply.github.com>
2024-11-14 19:41:59 +1100

    Merged main, xml

--- src/services/InputResolver.py
+++ src/services/InputResolver.py
@@ -2,7 +2,8 @@ import pathlib
 import json
 import csv
 import mimetypes
-from input_handlers import CSVHandler, JSONHandler, PDFHandler, PlaintextHandler, XMLHandler, JPEGHandler
+from input_handlers import CSVHandler, JSONHandler, PDFHandler, PlaintextHandler, XMLHandler, ELFHandler
+from elftools.elf.elffile import ELFFile
 import services.MutationHelper as mh
 from PIL import Image
 import xml.etree.ElementTree as ET
@@ -21,33 +22,32 @@ class InputResolver:
 
         with open(file_path, 'r') as file:
             content = file.read()
-            data_type = InputResolver._detect_data_type(content)
 
+        data_type = InputResolver._detect_data_type(content)
+        
         # Bit flips & byte flips
-        #general_mutations = [flip for flip in mh.flip(content.encode(), 200)]
+        general_mutations = [flip for flip in mh.flip(content.encode(), 200)]
         # Various empty strings
-        #general_mutations += [b'\x00', b'\n', b'', b'\r', b'\r\n']
+        general_mutations += [b'\x00', b'\n', b'', b'\r', b'\r\n']
         
         # Return fuzzed inputs, both format specific and general mutations
         if data_type == "csv":
-            #format_specific = CSVHandler.CsvHandler.parse_input(content)
-            format_specific = []
+            format_specific = CSVHandler.CsvHandler.parse_input(content)  
         elif data_type == "json":
-            #format_specific = JSONHandler.JSONHandler.parse_input(content)
-            format_specific = []
+            format_specific = JSONHandler.JSONHandler.parse_input(content)
         elif data_type == "pdf":
-            #format_specific = PDFHandler.PdfHandler.parse_input(content)
-            format_specific = []
+            format_specific = PDFHandler.PdfHandler.parse_input(content)
+        elif data_type == "elf":
+            format_specific = ELFHandler.ELFHandler.parse_input(content)
         elif data_type == "xml":
             format_specific = XMLHandler.XMLHandler.parse_input(content)
         elif data_type == None and mimetypes.guess_type(file_path)[0] == 'text/plain':
-            #format_specific = PlaintextHandler.PlaintextHandler.parse_input(content)
-            format_specific = []
+            format_specific = PlaintextHandler.PlaintextHandler.parse_input(content)
         else:
             print("I have no idea what file type this is lol")
             format_specific = []
 
-        return format_specific #+ general_mutations
+        return format_specific + general_mutations
 
     @staticmethod
     def _detect_data_type(content):
@@ -63,10 +63,16 @@ class InputResolver:
         except csv.Error:
             pass
 
+        try:
+            ELFFile(content)
+            return "elf"
+        except Exception as e:
+            pass
+
         # Check for PDF (not yet ready for the real world)
         if content.startswith('%PDF-'):
             return "pdf"
-        
+
         try:
             from io import StringIO
             file_like_object = StringIO(content)
